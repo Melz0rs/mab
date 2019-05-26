609b08d52337c9de58dd725aacf757a6792850e6
Carmel Shupak <carmelshupak@gmail.com>
2019-05-26 18:36:34 +0300

    redis

--- simple_proj_api/app.py
+++ simple_proj_api/app.py
@@ -1,9 +1,10 @@
 from flask import Flask
 import requests
+import redis
 
 app = Flask(__name__)
 worker_host = 'http://worker:5000'
-
+cache = redis.Redis(host='redis', port=6379)
 
 @app.route('/')
 def hello_world():
@@ -11,6 +12,11 @@ def hello_world():
     response = requests.get(worker_host)
     app.logger.info("hello world")
     return response.text
+
+
+@app.route('/notification/add')
+def add_notification():
+    cache.incr('notifications_count')
 
 
 if __name__ == '__main__':
